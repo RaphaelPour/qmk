4cf4acad827b6989cec1c5d744fa11a9391e83d0
Raphael Pour <info@raphaelpour.de>
2024-10-21 07:06:45 +0200

    beginning: set QUICK_TAP_TERM
    Set QUICK_TAP_TERM for bursting e.g. backspace.

--- keyboards/ferris/sweep/keymaps/beginning/config.h
+++ keyboards/ferris/sweep/keymaps/beginning/config.h
@@ -1,19 +1,4 @@
-/*
-Copyright 2020 Pierre Chevalier <pierrechevalier83@gmail.com>
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 #pragma once
 // Set the mouse settings to a comfortable speed/accuracy trade-off,
@@ -36,15 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
 #define QUICK_TAP_TERM 100
-
-uint16_t get_quick_tap_term(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
-      case KC_BACKSPACE:
-        return QUICK_TAP_TERM;
-      default:
-        return 0;
-    }
-}
+#define QUICK_TAP_TERM_PER_KEY 
 
 // Underglow configuration
 #ifdef RGBLIGHT_ENABLE
